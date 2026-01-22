00c27c275ad068fc6be7443c347cf40cd43672bd
Santo-Sandy <slantzer3112@gmail.com>
2026-01-22 19:52:04 +0530

    appbar add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
+import 'package:StoreLink/game.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,9 +15,12 @@ class MainApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Home',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 252, 200, 200),
+          ),
         ),
         home: MyHomePage(),
       ),
@@ -35,17 +38,210 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: MyAppBar(
+          title: Text(
+            "StoreLink",
+            style: Theme.of(context).primaryTextTheme.titleMedium,
+          ),
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: Column(
-        children: [Text('Click to see changes in console:'), Text(appState.current.asLowerCase),
+        children: [
+          Text('Click to see changes in console:'),
+          Text(appState.current.asLowerCase),
 
-        ElevatedButton(
+          ElevatedButton(
             onPressed: () {
               print('button pressed!');
             },
             child: Text('Click Me'),
           ),
+          Center(child: GamePage()),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: TextButton(onPressed: () {}, child: Text("+")),
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  final Widget title;
+  MyAppBar({required this.title, super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        margin: EdgeInsets.all(0.0),
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          color: const Color.fromARGB(255, 148, 225, 255),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.menu),
+              tooltip: 'Menu',
+              color: Colors.white,
+            ),
+
+            Expanded(child: title),
+
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.search, color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      curve: Curves.bounceIn,
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 2.5,
+                      vertical: 2.5,
+                    ),
+                    child: Tile(letter.char, letter.type),
+                  ),
+              ],
+            ),
+          GuessInput(
+            onSubmitGuess: (String guess) {
+              print(guess);
+              setState(() {
+                _game.guess(guess);
+              });
+            },
+          ),
         ],
       ),
     );
+  }
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (String input) {
+                onSubmitGuess(_textEditingController.text.trim());
+                _textEditingController.clear();
+                _focusNode.requestFocus();
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            onSubmitGuess(_textEditingController.text.trim());
+            _textEditingController.clear();
+            _focusNode.requestFocus();
+          },
+          padding: EdgeInsets.zero,
+          icon: Icon(Icons.arrow_circle_up),
+        ),
+      ],
+    );
+  }
+}
+
+class ExampleWidget extends StatefulWidget {
+  ExampleWidget({super.key});
+
+  @override
+  State<ExampleWidget> createState() => _ExampleWidgetState();
+}
+
+class _ExampleWidgetState extends State<ExampleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
